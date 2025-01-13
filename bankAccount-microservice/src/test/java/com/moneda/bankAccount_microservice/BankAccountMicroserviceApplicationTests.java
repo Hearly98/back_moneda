95f85f246143a2be55ad55bd5da3c99eaa17e617
Hearly98 <shuertas@gmail.com>
2025-01-12 19:15:04 -0500

    chore: initialize bankAccount-microservice project with Spring Boot

---
+++ bankAccount-microservice/src/test/java/com/moneda/bankAccount_microservice/BankAccountMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneda.bankAccount_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
