70945df89b3792d344151bf6bf1e924007b125e5
Hearly98 <shuertas981@gmail.com>
2025-02-08 19:12:34 -0500

    feat: add JPA query to find BankAccountType by code

--- bankAccount-microservice/src/main/java/com/moneda/bankAccount_microservice/common/bankAccountType/repositories/BankAccountTypeRepository.java
+++ bankAccount-microservice/src/main/java/com/moneda/bankAccount_microservice/common/bankAccountType/repositories/BankAccountTypeRepository.java
@@ -3,8 +3,11 @@ package com.moneda.bankAccount_microservice.common.bankAccountType.repositories;
 import com.moneda.bankAccount_microservice.common.bankAccountType.entities.BankAccountType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface BankAccountTypeRepository extends JpaRepository<BankAccountType, UUID> {
+    Optional<BankAccountType> findByCode(String code);
 }
