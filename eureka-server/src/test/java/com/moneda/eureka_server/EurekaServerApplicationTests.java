c85baee31f88f17f4b6e74f5b5c93f957b49ecf4
Hearly98 <shuertas981@gmail.com>
2025-02-16 02:46:34 -0500

    feat: initialize Eureka Server structure

---
+++ eureka-server/src/test/java/com/moneda/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneda.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
