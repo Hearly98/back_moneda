a43fc6a2ac3dd7dc6fa56cd91807e8016bb6e6a8
Hearly Sebastian Huertas Perez <116468255+Hearly98@users.noreply.github.com>
2025-03-01 02:32:52 -0500

    Revert "Feature/user microservice"

--- bankAccount-microservice/src/main/java/com/moneda/bankaccount/interbankidentifiertype/dto/CreateInterbankIdentifierTypeDto.java
+++
@@ -1,9 +0,0 @@
-package com.moneda.bankaccount.interbankidentifiertype.dto;
-import lombok.Data;
-
-@Data
-public class CreateInterbankIdentifierTypeDto {
-    private String code;
-    private String countryCode;
-    private String name;
-}
--- user-microservice/src/main/java/com/moneda/user_microservice/user/strategy/UserCountryService.java
+++
@@ -1,7 +0,0 @@
-package com.moneda.user_microservice.user.strategy;
-
-import com.moneda.user_microservice.user.entity.User;
-
-public interface UserCountryService {
-    User createUser(User user);
-}
