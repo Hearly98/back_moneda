3b83270cd34418e299f5341f6586d95d9bd193b9
Hearly98 <shuertas981@gmail.com>
2025-03-01 02:20:51 -0500

    feat: configure Eureka Client for User Microservice

--- user-microservice/src/main/java/com/moneda/user_microservice/UserMicroserviceApplication.java
+++ user-microservice/src/main/java/com/moneda/user_microservice/UserMicroserviceApplication.java
@@ -2,7 +2,9 @@ package com.moneda.user_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UserMicroserviceApplication {
 
